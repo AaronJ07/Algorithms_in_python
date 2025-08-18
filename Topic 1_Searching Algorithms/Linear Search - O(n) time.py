0a84651a5a6be40ffa98a569d31536f1502de582
Zhi Hong <66683692+AaronJ07@users.noreply.github.com>
2025-08-18 14:10:47 +0800

    Create Linear Search - O(n) time.py

---
+++ Topic 1_Searching Algorithms/Linear Search - O(n) time.py
@@ -0,0 +1,7 @@
+# Idea: Look at each item one by one until you find the target.
+
+# Step-by-step:
+# Start from index 0.
+# If arr[i] == target, return index.
+# If you reach the end without finding it, return -1.
+
